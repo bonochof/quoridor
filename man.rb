bb24a670b402f6992d29d1f1c5c1630d1fcaa256
bonochof <ryos427@yahoo.co.jp>
2017-10-24 22:24:51 +0900

    positionの初期値を書く部分を追加

--- man.rb
+++ man.rb
@@ -1,8 +1,27 @@
 # coding: utf-8
 
 class Man
-  def initialize
-    
+  attr_reader :x, :y
+  
+  def initialize( pnum )
+    case pnum
+    when 1
+      pos_x = 
+      pos_y = 
+    when 2
+      pos_x = 
+      pos_y =
+    when 3
+      pos_x = 
+      pos_y = 
+    when 4
+      pos_x = 
+      pos_y = 
+    else
+      abort "player number is wrong"
+    end
+    @x = pos_x
+    @y = pos_y
   end
   
   def movable?
