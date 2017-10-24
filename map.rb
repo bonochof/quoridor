acbc26f36371564f2f1a76c2f3ecd3edef24eca0
bonochof <ryos427@yahoo.co.jp>
2017-10-24 23:11:38 +0900

    attr_readerにwmapを追加し，initの引数を削除

--- map.rb
+++ map.rb
@@ -1,9 +1,9 @@
 # coding: utf-8
 
 class Map
-  attr_reader :map
+  attr_reader :map, :wmap
   
-  def initialize( pnum )
+  def initialize
     size = 9
     @map = Array.new( size ){ Array.new( size, 0 ) }
     @wmap = Array.new( size-1 ){ Array.new( size-1, 0 ) }
