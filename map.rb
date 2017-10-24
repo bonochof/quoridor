5981fc037a2134bb017f08b72348e7a821a7a562
bonochof <ryos427@yahoo.co.jp>
2017-10-25 01:43:21 +0900

    manをpawnに変更

--- map.rb
+++ map.rb
@@ -1,10 +1,10 @@
 # coding: utf-8
 
 class Map
-  attr_reader :man, :wall
+  attr_reader :pawn, :wall
   
   def initialize
-    @man = Array.new( $size ){ Array.new( $size, 0 ) }
+    @pawn = Array.new( $size ){ Array.new( $size, 0 ) }
     @wall = Array.new( $size-1 ){ Array.new( $size-1, 0 ) }
   end
 end
