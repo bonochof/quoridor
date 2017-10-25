5faeef436f2ba2f25e5b8c8ba25b5c2fc9786bd0
bonochof <ryos427@yahoo.co.jp>
2017-10-26 01:08:21 +0900

    壁マップのサイズ変更

--- src/map.rb
+++ src/map.rb
@@ -5,6 +5,6 @@ class Map
   
   def initialize
     @pawn = Array.new( $size ){ Array.new( $size, 0 ) }
-    @wall = Array.new( $size-1 ){ Array.new( $size-1, 0 ) }
+    @wall = Array.new( $size+1 ){ Array.new( $size+1, 0 ) }
   end
 end
