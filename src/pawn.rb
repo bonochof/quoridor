033bb5dedf5bcbdc91f307d402ec4b23494a219d
yamane <yamane0723@gmail.com>
2017-10-30 23:05:55 +0900

    mapに駒の位置を代入する処理を追加

--- src/pawn.rb
+++ src/pawn.rb
@@ -1,4 +1,4 @@
-# coding: utf-8
+﻿# coding: utf-8
 
 class Pawn
   attr_reader :x, :y, :num
@@ -6,23 +6,27 @@ class Pawn
   def initialize( pnum )
     case pnum
     when 1
-      pos_x = 8
-      pos_y = 4
+      pos_x = 16
+      pos_y = 8
     when 2
       pos_x = 0
-      pos_y = 4
-    when 3
-      pos_x = 4
       pos_y = 8
+    when 3
+      pos_x = 8
+      pos_y = 16
     when 4
-      pos_x = 4
+      pos_x = 8
       pos_y = 0
     else
       abort "player number is wrong"
     end
+    
     @num = pnum
     @x = pos_x
     @y = pos_y
+    
+    $map[@y][@x] = 2 if @num == 1
+    $map[@y][@x] = 3 if @num == 2
   end
   
   def movable?( dir )
@@ -30,17 +34,20 @@ class Pawn
     when :up
       return true if @y > 0
     when :down
-      return true if @y < $size - 1
+      return true if @y < $size*2 - 2
     when :left
       return true if @x > 0
     when :right
-      return true if @x < $size - 1
+      return true if @x < $size*2 - 2
     end
     
     return false
   end
   
   def move( dir )
+    $map[@y][@x] = 0 if $turn == 1
+    $map[@y][@x] = 0 if $turn == 2
+    
     case dir
     when :up
       @y -= 2
@@ -51,5 +58,8 @@ class Pawn
     when :right
       @x += 2
     end
+
+    $map[@y][@x] = 2 if $turn == 1
+    $map[@y][@x] = 3 if $turn == 2
   end
 end
